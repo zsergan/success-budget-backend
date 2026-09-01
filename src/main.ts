d4a9c9b587b2cd97678c189ac470e8f78ebdf892
Zakhar Sergan <56780779+ZSergan@users.noreply.github.com>
2026-09-01 07:48:46 -0500

    feat: add helmet security headers
    Sets the standard set of response headers (X-Frame-Options,
    X-Content-Type-Options, HSTS, etc.) that were previously missing
    entirely.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { BadRequestException, ValidationError, ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './shared/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
